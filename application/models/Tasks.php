60e581e858eff9e03cdd81ca052bdc916b838728
aavram <aavram27@gmail.com>
2018-03-20 16:22:40 -0700

    load method

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -11,12 +11,35 @@
  *
  * @author Lucas
  */
-class Tasks extends CSV_Model {
+class Tasks extends XML_Model {
 
         public function __construct()
         {
-                parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+                parent::__construct(APPPATH . '../data/tasks.xml', 'id');
         }
+        
+        function load(){
+            if (($tasks = simplexml_load_file($this->_origin)) !== FALSE)
+		{
+			foreach ($tasks as $task) {
+				$record = new stdClass();
+				$record->id = (int) $task['id'];
+				$record->task = (string) $task->task;
+				$record->priority = (int) $task->priority;
+				$record->size = (int) $task->size;
+				$record->group = (int) $task->group;
+				$record->deadline = (string) $task->deadline;
+				$record->status = (int) $task->status;
+				$record->flag = (int) $task->flag;
+
+				$this->_data[$record->id] = $record;
+			}
+		}
+
+		// rebuild the keys table
+		$this->reindex();
+        }
+        
         
         function getCategorizedTasks()
         {
